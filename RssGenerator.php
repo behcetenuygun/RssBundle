5269230ea3ee9354236077c9b9f2e6b227c0df6a
Behcet Mutlu <behcet.mutlu@enuygun.com>
2012-05-31 14:34:23 +0300

    Image description and link are included

--- RssGenerator.php
+++ RssGenerator.php
@@ -53,7 +53,7 @@ class RssGenerator
         $height->appendChild($this->dom->createTextNode('55'));
 
         $description = $this->dom->createElement('description');
-        $description->appendChild($this->dom->createTextNode('Enuygun.com Bilgi'));
+        $description->appendChild($this->dom->createTextNode('Enuygun.com Güncel Makaleler'));
 
         $link = $this->dom->createElement('link');
         $link->appendChild($this->dom->createTextNode($this->configs['link']));
